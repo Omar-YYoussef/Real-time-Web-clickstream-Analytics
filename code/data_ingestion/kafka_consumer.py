ffa3eb0d12b6428f8b0c66519410def83677dbd3
khaaaleed-5 <khaled.20210298@gmail.com>
2023-12-09 16:57:44 +0200

    database

--- code/data_ingestion/kafka_consumer.py
+++ code/data_ingestion/kafka_consumer.py
@@ -1,9 +1,11 @@
-from ..data_processing.analytics_class import Analytics
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from re import A
 import pymysql
+sys.path.append(r'\Real-time-Web-clickstream-Analytics\code')
+
+from data_processing.analytics_class import Analytics
 
 # Create a Spark session
 spark = SparkSession.builder \
@@ -42,41 +44,48 @@ df = spark.readStream \
 
 # Cast the value column to string
 df = df.select("data.*")
-df = Analytics.all_analytics(df)
+# df = Analytics.all_analytics(df)
 
-# PROCESSING
 def insert_into_db(row):
     # Define the connection details for your PHPMyAdmin database
     host = "p3nlmysql47plsk.secureserver.net"
     port = 3306
-    database = "big_data"
-    username = "root"
-    password = ""
-    
-    conn = pymysql.connect(host=host, port=port, user=username, passwd=password, db=database)
+    database = "Clickstream_DB"
+    username = "bigdata"
+    password = "hytham123"
+
+    conn = pymysql.connect(host=host, port=port, user=username, password=password, db=database)
     cursor = conn.cursor()
 
     # Extract the required columns from the row
-    column1_value = row.name
-    column2_value = row.age
+    column1_value = row.user_id
+    column2_value = row.Session_Start_Time
+    column3_value = row.Page_URL
+    column4_value = row.Timestamp
+    column5_value = row.Duration_on_Page_s
+    column6_value = row.Interaction_Type
+    column7_value = row.Device_Type
+    column8_value = row.Browser
+    column9_value = row.Country
+    column10_value = row.Referrer
 
     # Prepare the SQL query to insert data into the table
-    sql_query = f"INSERT INTO user (name, age) VALUES ('{column1_value}', '{column2_value}')"
+    sql_query = f"INSERT INTO DataSet (user_id, Session_Start_Time, Page_URL, Timestamp, Duration_on_Page_s, Interaction_Type, Device_Type, Browser, Country, Referrer) VALUES ('{column1_value}', '{column2_value}','{column3_value}','{column4_value}','{column5_value}','{column6_value}','{column7_value}','{column8_value}','{column9_value}','{column10_value}')"
     
-    # Execute the SQL query
+# Execute the SQL query
     cursor.execute(sql_query)
 
     # Commit the changes
     conn.commit()
     conn.close()
 
-
 # Write to console
 query = df.writeStream \
     .outputMode("append") \
+    .foreach(insert_into_db) \
     .format("console") \
     .start()
-    # .foreach(insert_into_db) \
 
 # Wait for query termination
+
 query.awaitTermination()
